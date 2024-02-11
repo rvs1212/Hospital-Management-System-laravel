91b8ef24506a48f6e739c7c107b31fc042ce15a2
Reni Stephen <renistephen2@gmail.com>
2024-02-11 00:57:18 -0500

    adding modified code

--- resources/views/user/appointment.blade.php
+++ resources/views/user/appointment.blade.php
@@ -16,7 +16,6 @@
           <div class="col-12 col-sm-6 py-2 wow fadeInLeft" data-wow-delay="300ms">
             <input type="date" name="date" class="form-control">
           </div>
-          
           <div class="col-12 col-sm-6 py-2 wow fadeInLeft" data-wow-delay="300ms">
             <input type="time" name="date" class="form-control">
           </div>
@@ -36,9 +35,7 @@
           <div class="col-12 col-sm-6 py-2 wow fadeInUp" data-wow-delay="300ms">
             <input type="text" name="number" class="form-control" placeholder="Number..">
           </div>
-          <!-- <div class="col-12 col-sm-6 py-2 wow fadeInUp" data-wow-delay="300ms">
-            <input type="text" name="number" class="form-control" placeholder="Number..">
-          </div> -->
+         
           <div class="col-12 py-2 wow fadeInUp" data-wow-delay="300ms">
             <textarea name="message" id="message" class="form-control" rows="6" placeholder="Enter message.."></textarea>
           </div>
